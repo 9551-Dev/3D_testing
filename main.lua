6ac635e9b2eb6529c2e9bc74702e8a8b318ea24a
9551Dev <oliverekcaha@gmail.com>
2022-10-16 01:07:45 +0200

    added and fixed a lot of stuff

--- main.lua
+++ main.lua
@@ -1,8 +1,42 @@
 local s = 0.5
 
-local half_pi = math.pi / 2
-
 local mat = require("mat")
+
+local function u(u)
+    return (u+1)/3
+end
+local function v(v)
+    return v/4
+end
+
+--[[local objects = {
+    {
+        verts={
+            -s,-s,-s, u(1) ,v(0),
+            s,-s,-s,  u(0) ,v(0),
+            -s,s,-s,  u(1) ,v(1),
+            s,s,-s,   u(0) ,v(1),
+            -s,-s,s,  u(1) ,v(3),
+            s,-s,s,   u(0) ,v(3),
+            -s,s,s,   u(1) ,v(2),
+            s,s,s,    u(0) ,v(2),
+            -s,-s,-s, u(1) ,v(4),
+            s,-s,-s,  u(0) ,v(4),
+            -s,-s,-s, u(2) ,v(1),
+            -s,-s,s,  u(2) ,v(2),
+            s,-s,-s,  u(-1),v(1),
+            s,-s,s,   u(-1),v(2),
+        },
+        indices={
+            1,3,2,   3,4,2,
+            5,9,6,   6,9,10,
+            3,7,4,   4,7,8,
+            5,6,8,   5,8,7,
+            3,11,12, 3,12,7,
+            13,4,8,  13,8,14
+        }
+    },
+}]]
 
 local objects = {
     {
@@ -27,14 +61,14 @@ local objects = {
     },
 }
 
-local res = 2
+local res = 10
 
-local w,h = love.graphics.getDimensions()
-w = w / res
-h = h / res
-local scale_factor = math.min(w,h)/2
+local worg,horg = love.graphics.getDimensions()
+love.window.setMode(worg,horg,{resizable=true})
+local w = worg / res
+local h = horg / res
 
-local diff = math.max(w,h)/2-scale_factor
+local const = 0
 
 local function getT(a,b,p)
     local v1 = {a[1]-b[1], a[2]-b[2]}
@@ -43,34 +77,18 @@ local function getT(a,b,p)
     return (v1[1]*v2[1] + v1[2]*v2[2]) / (v1[1]*v1[1] + v1[2]*v1[2])
 end
 
-local function cull(a,b,c)
-    local i1,i2,i3 = a[1],a[2],a[3]
-
-    local a1 = b[1]-i1
-    local a2 = b[2]-i2
-    local a3 = b[3]-i3
-
-    local b1 = c[1]-i1
-    local b2 = c[2]-i2
-    local b3 = c[3]-i3
-
-    return (a2*b3 - a3*b2)*a[1] +
-        (a3*b1 - a1*b3)*c[2]*a[2] +
-        (a1*b2 - a2*b1)*c[3]*a[3]
-end
-
 local function intY(a,b,y)
     local oy = (b[2] - a[2])
-    if oy == 0 then oy = half_pi end
+    if oy == 0 then oy = 5e-50 end
 
     return a[1] + ((y - a[2]) * (b[1] - a[1])) / oy
 end
 
-local function transform_screen_space(p)
+local function transform_screen_space(p,w,h)
     local inverse_z = 1/p[4]
     return {
-        ( p[1]*inverse_z+1)*scale_factor+diff,
-        (-p[2]*inverse_z+1)*scale_factor,
+        ( p[1]*inverse_z+1)*w/2,
+        (-p[2]*inverse_z+1)*h/2,
         inverse_z,
         p[4],
         p[5]*inverse_z,
@@ -78,14 +96,15 @@ local function transform_screen_space(p)
     }
 end
 
-local function makePerspective(n, f, fov)
-    local aspectRatio = 1
+local function makePerspective(w,h,n,f,fov)
+    local aspectRatio = 1/w * h
     fov = math.rad(fov)
+
     return mat.new(4,4,
         aspectRatio/math.tan(fov*0.5),0,0,0,
         0,1/(math.tan(fov*0.5)),0,0,
-        0,0,-f/(f-n),-f*n/(f-n),
-        0,0,-1,1
+        0,0,-f/(f-n),-1,
+        0,0,-f*n/(f-n),1
     )
 end
 
@@ -110,7 +129,7 @@ local function create_rotation_eulers(eulers)
     )
 end
 
-local per = makePerspective(5, 1, 45)
+local per
 
 local CEIL,MAX,MIN,FLOOR = math.ceil,math.max,math.min,math.floor
 
@@ -161,12 +180,15 @@ local function draw_flat_top_triangle(v0,v1,v2,tex,origin,fragment)
             local bary = get_bary_coord(x,y,v0,v1,v2)
             local tpos = interpolate_uv(bary,v0,v1,v2)
 
-            local t3 = (x - sx_start) / (sx_end - sx_start)
+            local div = sx_end - sx_start
+            local t3 = (x - sx_start) / ((div == 0) and 1 or div)
+
+
             local z = 1/lerp(w1,w2,t3)
 
             fragment(x,y,lerp(z1,z2,t3),
-                MAX(0,MIN(FLOOR(tpos[1]*z*tex.w-1),tex.w-1)),
-                MAX(0,MIN(FLOOR(tpos[2]*z*tex.h-1),tex.h-1))
+                MAX(0,MIN(CEIL(tpos[1]*z*tex.w),tex.w-1)),
+                MAX(0,MIN(CEIL(tpos[2]*z*tex.h),tex.h-1)),{t3=t3,u=tpos[1],v=tpos[2]}
             )
         end
     end
@@ -200,12 +222,14 @@ local function draw_flat_bottom_triangle(v0,v1,v2,tex,origin,fragment)
             local bary =  get_bary_coord(x,y,v0,v1,v2)
             local tpos =  interpolate_uv(bary,v0,v1,v2)
 
-            local t3 = (x - sx_start) / (sx_end - sx_start)
+            local div = sx_end - sx_start
+            local t3 = (x - sx_start) / ((div == 0) and 1 or div)
+
             local z = 1/lerp(w1,w2,t3)
 
             fragment(x,y,lerp(z1,z2,t3),
-                MAX(0,MIN(FLOOR(tpos[1]*z*tex.w-1),tex.w-1)),
-                MAX(0,MIN(FLOOR(tpos[2]*z*tex.h-1),tex.h-1))
+                MAX(0,MIN(CEIL(tpos[1]*z*tex.w),tex.w-1)),
+                MAX(0,MIN(CEIL(tpos[2]*z*tex.h),tex.h-1)),{t3=t3,u=tpos[1],v=tpos[2]}
             )
         end
     end
@@ -245,7 +269,8 @@ end
 
 local tex
 function love.load()
-    tex = love.image.newImageData("shrek.png")
+    tex = love.image.newImageData("jesus.png")
+    per = makePerspective(w,h,-0.5,90,90)
 end
 
 local function printt(t)
@@ -264,8 +289,101 @@ local scale = mat.new(4,4,
 )
 
 local die = false
+local selected_pixels = {}
+
+local function render_pixel(screen,x,y,z,tx,ty,debug)
+    local r,g,b,a = tex:getPixel(tx,ty)
+    --local r,g,b,a = debug.u,0,debug.v,1
+
+    if a > 0.5 then
+
+        if not screen[y] then screen[y] = {} end
+        if screen[y] and screen[y][x] and screen[y][x].w and screen[y][x].w > z then
+            if selected_pixels[x] and selected_pixels[x][y] then
+                screen[y][x] = {w=0,1,0,0,1}
+                print(debug.t3)
+            else
+                screen[y][x] = {w=z,r,g,b,1}
+            end
+        elseif not screen[y][x] then
+            if selected_pixels[x*res] and selected_pixels[x][y] then
+                screen[y][x] = {w=0,1,0,0,1}
+            else
+                screen[y][x] = {w=z,r,g,b,1}
+            end
+        end
+
+    end
+end
+
+local function interpolate_vertex(v1,v2,alpha)
+    return {
+        (1 - alpha) * v1[1] + alpha * v2[1],
+        (1 - alpha) * v1[2] + alpha * v2[2],
+        (1 - alpha) * v1[3] + alpha * v2[3],
+        (1 - alpha) * v1[4] + alpha * v2[4],
+        (1 - alpha) * v1[5] + alpha * v2[5],
+        (1 - alpha) * v1[6] + alpha * v2[6],
+    }
+end
+
+local function clip_1_vertex(tris,v1,v2,v3)
+    local alpha1 = (-v1[3]) / (v2[3]-v1[3])
+    local alpha2 = (-v1[3]) / (v3[3]-v1[3])
+
+    local v10 = interpolate_vertex(v1,v2,alpha1)
+    local v01 = interpolate_vertex(v1,v3,alpha2)
+
+    tris[#tris+1] = {v10,v2,v3,split=true}
+    tris[#tris+1] = {v10,v01,v3,split=true}
+end
+
+local function clip_2_vertices(tris,v1,v2,v3)
+    local alpha1 = (-v1[3]) / (v3[3]-v1[3])
+    local alpha2 = (-v2[3]) / (v3[3]-v2[3])
+
+    local v10 = interpolate_vertex(v1,v3,alpha1)
+    local v01 = interpolate_vertex(v2,v3,alpha2)
+
+    tris[#tris+1] = {v10,v01,v3,split=true}
+end
+
+local function handle_triangle(tri_list,a,b,c)
+    local v1x,v1y,v1z,v1w = a[1],a[2],a[3],a[4]
+    local v2x,v2y,v2z,v2w = b[1],b[2],b[3],b[4]
+    local v3x,v3y,v3z,v3w = c[1],c[2],c[3],c[4]
+
+    if v1x >  v1w and v2x >  v2w and v3x >  v3w then return end
+    if v1x < -v1w and v2x < -v2w and v3x < -v3w then return end
+    if v1y >  v1w and v2y >  v2w and v3y >  v3w then return end
+    if v1y < -v1w and v2y < -v2w and v3y < -v3w then return end
+    if v1z >  v1w and v2z > -v2w and v3z >  v3w then return end
+    if v1z < 0    and v2z < 0    and v3z < 0    then return end
+
+    if v1z < 0 then
+        if v2z < 0 then
+            clip_2_vertices(tri_list,a,b,c)
+        elseif v3z < 0 then
+
+            clip_2_vertices(tri_list,a,c,b)
+        else
+            clip_1_vertex(tri_list,a,b,c)
+        end
+    elseif v2z < 0 then
+        if v3z < 0 then
+            clip_2_vertices(tri_list,b,c,a)
+        else
+            clip_1_vertex(tri_list,b,a,c)
+        end
+    elseif v3z < 0 then
+        clip_1_vertex(tri_list,c,a,b)
+    else
+        tri_list[#tri_list+1] = {a,b,c}
+    end
+end
 
 function love.draw()
+    local per = makePerspective(w,h,-0.5,90,50)
     local screen = {}
 
     for k,v in pairs(objects) do
@@ -277,8 +395,6 @@ function love.draw()
         local n = 0
         for i=1,#vert,5 do
             n = n + 1
-
-            local v = {}
 
             local scaled_vertice = mat.vector(
                 vert[i],vert[i+1],vert[i+2],1
@@ -294,36 +410,35 @@ function love.draw()
                 1,0,0,0,
                 0,1,0,0,
                 0,0,1,0,
-                px,py,pz-4,1
+                px,py,pz,1
             )
 
-            vertices[n] = model*per
+            local v = model*per
 
-            vertices[n][5] = vert[i+3]
-            vertices[n][6] = vert[i+4]
+            v[5] = vert[i+3]
+            v[6] = vert[i+4]
+
+            vertices[n] = v
         end
 
+        local triangles = {}
+
         for i=1,#tri,3 do
-            local a,b,c = vertices[tri[i]],vertices[tri[i+1]],vertices[tri[i+2]]
+            n = n + 1
 
+            handle_triangle(triangles,vertices[tri[i]],vertices[tri[i+1]],vertices[tri[i+2]])
+        end
+
+        for i=1,#triangles do
+            local t = triangles[i]
+            local a,b,c = t[1],t[2],t[3]
             raster_triangle(
-                transform_screen_space(a),
-                transform_screen_space(b),
-                transform_screen_space(c),
+                transform_screen_space(a,w,h),
+                transform_screen_space(b,w,h),
+                transform_screen_space(c,w,h),
                 {tex=tex,w=tex:getWidth(),h=tex:getHeight()},
-                function(x,y,z,tx,ty)
-                    local r,g,b,a = tex:getPixel(tx,ty)
-
-                    if a > 0.5 then
-                    
-                        if not screen[y] then screen[y] = {} end
-                        if screen[y] and screen[y][x] and screen[y][x].w and screen[y][x].w < z then
-                            screen[y][x] = {w=z,r,g,b,a}
-                        elseif not screen[y][x] then
-                            screen[y][x] = {w=z,r,g,b,a}
-                        end
-
-                    end
+                function(x,y,z,tx,ty,debug)
+                    render_pixel(screen,x,y,z,tx,ty,debug)
                 end
             )
         end
@@ -356,10 +471,10 @@ function love.update(dt)
         ry = ry + 100*dt
     end
     if love.keyboard.isDown("a") then
-        px = px + 2*dt
+        px = px - 2*dt
     end
     if love.keyboard.isDown("d") then
-        px = px - 2*dt
+        px = px + 2*dt
     end
     if love.keyboard.isDown("w") then
         pz = pz - 2*dt
@@ -368,12 +483,30 @@ function love.update(dt)
         pz = pz + 2*dt
     end
     if love.keyboard.isDown("lctrl") then
-        py = py + 2*dt
+        py = py - 2*dt
     end
     if love.keyboard.isDown("lshift") then
-        py = py - 2*dt
+        py = py + 2*dt
     end
     if love.keyboard.isDown("space") then
         die = true
     end
+end
+
+function love.mousepressed(x,y,b)
+    local x,y = math.floor(x/res),math.floor(y/res)
+    if not selected_pixels[x] then selected_pixels[x] = {} end
+    selected_pixels[x][y] = b ~= 2
+end
+
+function love.wheelmoved(dx,dy)
+end
+
+function love.resize()
+    local res = 10
+    worg,horg = love.graphics.getDimensions()
+    w = worg / res
+    h = horg / res
+
+    per = makePerspective(w,h,-0.5,90,50)
 end
